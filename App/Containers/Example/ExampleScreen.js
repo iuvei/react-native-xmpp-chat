c3474080a2b99f838f3546c0d746a1363dcee1ab
Nicolas Manzato <nicolasmanzato@MacBook-Pro-de-Nicolas.local>
2019-04-21 15:45:34 -0300

    initial commit

---
+++ App/Containers/Example/ExampleScreen.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Platform, Text, View, Button, ActivityIndicator, Image } from 'react-native'
+import { connect } from 'react-redux'
+import { PropTypes } from 'prop-types'
+import ExampleActions from 'App/Stores/Example/Actions'
+import { liveInEurope } from 'App/Stores/Example/Selectors'
+import Style from './ExampleScreenStyle'
+import { Images } from 'App/Theme'
+
+class ExampleScreen extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser()
+  }
+
+  render() {
+    return (
+      <View style={Style.container}>
+        <Text>Hola :)</Text>
+      </View>
+    )
+  }
+}
+
+ExampleScreen.propTypes = {
+  user: PropTypes.object,
+  userIsLoading: PropTypes.bool,
+  userErrorMessage: PropTypes.string,
+  fetchUser: PropTypes.func,
+  liveInEurope: PropTypes.bool,
+}
+
+const mapStateToProps = (state) => ({
+  user: state.example.user,
+  userIsLoading: state.example.userIsLoading,
+  userErrorMessage: state.example.userErrorMessage,
+  liveInEurope: liveInEurope(state),
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchUser: () => dispatch(ExampleActions.fetchUser()),
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ExampleScreen)
---
+++ App/Containers/Example/ExampleScreenStyle.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native'
+import Fonts from 'App/Theme/Fonts'
+import ApplicationStyles from 'App/Theme/ApplicationStyles'
+
+export default StyleSheet.create({
+  container: {
+    ...ApplicationStyles.screen.container,
+  },
+})
